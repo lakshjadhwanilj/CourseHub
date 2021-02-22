35761da7c410f4ded47b7b0f3eb74505e6bceb88
lakshjadhwanilj <lakshjadhwani.lj@gmail.com>
2021-02-22 23:58:10 +0530

    Connecting to MongoDB, added colors package.

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
 // Requiring packages
 import express from 'express'
 import dotenv from 'dotenv'
+import colors from 'colors'
+import connectDB from './config/db.js'
 import courses from './data/courses.js'
 
 // Load config
 dotenv.config()
+
+// Connecting to MongoDB
+connectDB()
 
 // Initialize app
 const app = express()
@@ -25,4 +30,4 @@ app.get('/api/courses/:id', (req, res) => {
 
 // Listening to the port
 const PORT = process.env.PORT || 5000
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))

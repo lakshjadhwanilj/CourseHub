0feb9327a595381c02a51e28db59d9dd8e430087
lakshjadhwanilj <lakshjadhwani.lj@gmail.com>
2021-02-22 19:31:55 +0530

    Environment variables setup

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
 // Requiring packages
 const express = require('express')
+const dotenv = require('dotenv')
 const courses = require('./data/courses')
+
+// Load config
+dotenv.config()
 
 // Initialize app
 const app = express()
@@ -20,4 +24,5 @@ app.get('/api/courses/:id', (req, res) => {
 })
 
 // Listening to the port
-app.listen(5000, console.log('Server running on port 5000'))
+const PORT = process.env.PORT || 5000
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))

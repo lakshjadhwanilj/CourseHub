6fbe5e41276ac2fdd2aab95669b900faa2807ca7
lakshjadhwanilj <lakshjadhwani.lj@gmail.com>
2021-02-23 18:41:15 +0530

    created a Redux store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import store from './store'
 // CSS
 import './assets/bootstrap.min.css'
 import './index.css'
 // Components
 import App from './App'
 
-ReactDOM.render( <App />,
+ReactDOM.render(
+  <Provider>
+    <App />
+  </Provider>,
   document.getElementById('root')
 )
---
+++ frontend/src/store.js
@@ -0,0 +1,13 @@
+import { createStore, combineReducres, applyMiddleware, combineReducers } from 'redux'
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
+
+const reducer = combineReducers({})
+
+const initialState = {}
+
+const middleware = [thunk]
+
+const store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware(...middleware)))
+
+export default store

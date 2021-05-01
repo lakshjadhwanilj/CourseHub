25f449f9d449c7ad0ac3242c19d57cef4043ad27
lakshjadhwanilj <lakshjadhwani.lj@gmail.com>
2021-05-01 18:31:22 +0530

    Image Upload

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,12 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
+import path from 'path'
 import connectDB from './config/db.js'
 import courseRoutes from './routes/courseRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
+import uploadRoutes from './routes/uploadRoutes.js'
 import { errorHandler, notFound } from './middleware/errorMiddleware.js'
 
 // Load config
@@ -27,8 +29,12 @@ app.get('/', (req, res) => {
 app.use('/api/courses', courseRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
+app.use('/api/upload', uploadRoutes)
 
 app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID))
+
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 // Custom Middleware
 app.use(notFound)
--- frontend/src/screens/CourseEditScreen.js
+++ frontend/src/screens/CourseEditScreen.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import { Link } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { listCourseDetails, updateCourse } from '../actions/courseActions'
@@ -20,6 +21,7 @@ const CourseEditScreen = ({ match, history }) => {
     const [category, setCategory] = useState('')
     const [availability, setAvailability] = useState('not available')
     const [description, setDescription] = useState('')
+    const [uploading, setUploading] = useState(false)
 
     const dispatch = useDispatch()
 
@@ -47,6 +49,27 @@ const CourseEditScreen = ({ match, history }) => {
             }
         }
     }, [course, courseId, dispatch, history, successUpdate])
+
+    const uploadFileHandler = async (e) => {
+        const file = e.target.files[0]
+        const formData = new FormData()
+        formData.append('image', file)
+        setUploading(true)
+
+        try {
+            const config = {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            }
+            const { data } = await axios.post('/api/upload', formData, config)
+            setImage(data)
+            setUploading(false)
+        } catch (error) {
+            console.log(error)
+            setUploading(false)
+        }
+    }
 
     const submitHandler = (e) => {
         e.preventDefault()
@@ -104,6 +127,14 @@ const CourseEditScreen = ({ match, history }) => {
                                         value={image}
                                         onChange={(e) => setImage(e.target.value)}>
                                     </Form.Control>
+                                </Form.Group>
+                                <Form.Group controlId='image'>
+                                <Form.File
+                                        id='image-file'
+                                        label='Choose File'
+                                        custom
+                                        onChange={uploadFileHandler}></Form.File>
+                                    {uploading && <Loader />}
                                 </Form.Group>
                                 
                                 <Form.Group controlId='instructor'>
